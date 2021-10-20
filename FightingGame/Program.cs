b13489cf7d7f03d10865188ab19f828532223d45
Alexander Sundvisson <nitrogaplays@gmail.com>
2021-10-20 13:14:22 +0200

    first version

---
+++ FightingGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+Random rnd = new Random();
+
+int Health = 100;
+int EnemyHealth = 100;
+
+Console.WriteLine("Welcome to this Fighting Game\nPlease choose a name for your fighter");
+
+string name = Console.ReadLine();
+string enemy = "Enemy";
+
+Console.WriteLine($"\nFight:\n{name} vs {enemy}!\n");
+
+while (Health > 0 && EnemyHealth > 0)
+{
+    int damage = rnd.Next(16);
+    EnemyHealth -= damage;
+    if (EnemyHealth <= 0)
+    {
+        EnemyHealth = 0;
+        damage = 0;
+        Console.WriteLine($"{enemy} is now dead!");
+    }
+    else
+    {
+        Console.WriteLine($"Enemy's health is now {EnemyHealth}");
+        damage = rnd.Next(16);
+        Health -= damage;
+        if (Health <= 0)
+    {
+        Health = 0;
+        Console.WriteLine("You died :(");
+    }
+    else
+    {
+        Console.WriteLine($"Your health is now {Health}");
+    }
+    }
+}
+
+if (Health > 0 && EnemyHealth == 0)
+{
+    Console.WriteLine($"\n{name} won with {Health} health left! :)");
+}
+else if (Health == 0 && EnemyHealth > 0)
+{
+    Console.WriteLine($"\n{enemy} won with {EnemyHealth} health left! :(");
+}
+
+Console.ReadLine();
